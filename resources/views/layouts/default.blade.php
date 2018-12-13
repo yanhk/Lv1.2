c3300b98b796c69807a032917c1d1ca306f346af
Yanhk <705177961@qq.com>
2018-12-13 18:02:18 +0800

    优化页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,17 @@
 <html>
 <head>
     <title>@yield('title', '首页') - 燕南飞</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,19 @@
 @extends('layouts.default')
+
 @section('content')
-    <h1>首页</h1>
+    <div class="jumbotron">
+        <h1>Mo</h1>
+        <p class="lead">
+            燕南天
+        </p>
+        <p>
+            行侠仗义，武功天下第一
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
